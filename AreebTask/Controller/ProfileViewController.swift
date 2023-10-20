f86d2fdefc7998859826169495bd54084ce6f70e
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-10-21 02:07:03 +0300

    configure UI with data

--- AreebTask/Controller/ProfileViewController.swift
+++ AreebTask/Controller/ProfileViewController.swift
@@ -6,24 +6,43 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ProfileViewController: UIViewController {
-
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var ownerNameLabel: UILabel!
+    
+    @IBOutlet weak var repoNameLabel: UILabel!
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    var date: String?
+    var repository: Repository?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configureUIWithData()
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureUIWithData(){
+        
+        guard let imageUrl = repository?.owner?.avatarUrl?.asUrl else {return}
+        profileImageView.sd_setImage(with: imageUrl)
+        profileImageView.makeCircular()
+        ownerNameLabel.text = repository?.owner?.login
+        guard let is_private = repository?.privateField else {return}
+        if is_private {
+            statusLabel.text = "Private"
+        }else{
+            statusLabel.text = "Public"
+        }
+        repoNameLabel.text = repository?.name
     }
-    */
 
 }
