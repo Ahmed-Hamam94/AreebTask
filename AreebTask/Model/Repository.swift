701c3ee1bca18fbf1095b98557e64e78ca4e08ee
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-10-20 04:09:34 +0300

    optional struct

--- AreebTask/Model/Repository.swift
+++ AreebTask/Model/Repository.swift
@@ -8,18 +8,36 @@
 import Foundation
 
 struct Repository: Codable {
-    
-    let id: Int
-    let name: String
-    let fullName: String
-    let privateField: Bool
-    let owner: Owner
-    let description: String
+
+    let id: Int?
+    let name: String?
+    let fullName: String?
+    let privateField: Bool?
+    let owner: Owner?
+    let description: String?
+
+    private enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+        case fullName = "full_name"
+        case privateField = "private"
+        case owner = "owner"
+        case description = "description"
+    }
 }
 
 struct Owner: Codable {
-    
-    let login: String
-    let id: Int
-    let avatarUrl: String
+
+    let login: String?
+    let id: Int?
+    let avatarUrl: String?
+
+    private enum CodingKeys: String, CodingKey {
+        case login = "login"
+        case id = "id"
+        case avatarUrl = "avatar_url"
+    }
+
 }
+
+
