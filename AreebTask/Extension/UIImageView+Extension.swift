a073e9147fa9643ba7947bec549257ba5117c5a9
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-10-21 02:04:51 +0300

    add func to make image circular

--- AreebTask/Extension/UIImageView+Extension.swift
+++ AreebTask/Extension/UIImageView+Extension.swift
@@ -5,4 +5,12 @@
 //  Created by Ahmed Hamam on 20/10/2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+    
+    func makeCircular(){
+        self.layer.cornerRadius = self.frame.width/2
+
+    }
+}
