bc83b9cf23c1aa5a1519f5763e88de06f8107b11
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-10-21 02:03:04 +0300

    add repos endpoint to fetch date

--- AreebTask/NetworkManager/EndPoint.swift
+++ AreebTask/NetworkManager/EndPoint.swift
@@ -10,16 +10,19 @@ import Foundation
 enum EndPoint {
     
     static let base_url = "https://api.github.com/"
-   
+   //https://api.github.com/users/mojombo
     
     case repositories
- 
+    case repos(String)
     
 
     var path: String{
         switch self{
         case .repositories:
             return "repositories"
+            
+        case .repos(let full_name):
+            return "repos/\(full_name)"
        
         }
 
