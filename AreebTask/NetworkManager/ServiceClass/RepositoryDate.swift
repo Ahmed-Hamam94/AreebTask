67a438b83ba451a082b15735a4aa5309bd27548f
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-10-21 02:00:27 +0300

    add protocol and class to fetch date

--- AreebTask/NetworkManager/ServiceClass/RepositoryDate.swift
+++ AreebTask/NetworkManager/ServiceClass/RepositoryDate.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+protocol RepositoryDateProtocol {
+    
+    func fetchRepositories(fullName: String ,completion: @escaping (Result<Repository, APIError>)-> Void)
+}
+
+class RepositoryDateService: RepositoryDateProtocol {
+    
+    func fetchRepositories(fullName: String, completion: @escaping (Result<Repository, APIError>) -> Void) {
+        
+        NetworkManager.shared.request(endPoint: EndPoint.repos(fullName), method: .Get, completion: completion)
+    }
+    
+    
+}
