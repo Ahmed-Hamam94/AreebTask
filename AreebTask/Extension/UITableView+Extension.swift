434fc6d0d5f9157c90c483ef150f4a06db4a00d4
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-10-20 04:10:21 +0300

    add register func for cell

--- AreebTask/Extension/UITableView+Extension.swift
+++ AreebTask/Extension/UITableView+Extension.swift
@@ -5,4 +5,11 @@
 //  Created by Ahmed Hamam on 20/10/2023.
 //
 
-import Foundation
+import UIKit
+
+extension UITableView {
+ /// register TableViewCell Xib
+    func registerCelNib<cell: UITableViewCell>(cellClass: cell.Type){
+        self.register(UINib(nibName: String(describing: cell.self), bundle: nil), forCellReuseIdentifier: String(describing: cell.self))
+    }
+}
