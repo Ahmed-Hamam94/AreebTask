a0287ed0fb24b1acd1c4b230094ad0f0c88d8d42
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-10-20 04:06:12 +0300

    add service class and protocol

--- AreebTask/NetworkManager/ServiceClass/Repositories.swift
+++ AreebTask/NetworkManager/ServiceClass/Repositories.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+protocol RepositoriesProtocol {
+    
+    func fetchRepositories(completion: @escaping (Result<[Repository], APIError>)-> Void)
+}
+
+class RepositoriesService: RepositoriesProtocol {
+    
+    func fetchRepositories(completion: @escaping (Result<[Repository], APIError>) -> Void) {
+        
+        NetworkManager.shared.request(endPoint: EndPoint.repositories, method: .Get, completion: completion)
+    }
+    
+    
+}
